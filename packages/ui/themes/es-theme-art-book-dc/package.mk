f591fed94743bf60fff89fd02ff0084429d4a949
uzucore <byunjaeil@gmail.com>
2024-11-05 13:13:22 +0900

    Update package.mk

--- packages/ui/themes/es-theme-art-book-dc/package.mk
+++ packages/ui/themes/es-theme-art-book-dc/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2021 Fewtarius
 
 PKG_NAME="es-theme-art-book-dc"
-PKG_VERSION="9f42781abb048b6cf57ccd40ce3cba97882573f1"
+PKG_VERSION="22547d6e7c76a16954ec41f53154ea83b200aec3"
 PKG_ARCH="any"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/UzuCore/es-theme-art-book-rocknix"
