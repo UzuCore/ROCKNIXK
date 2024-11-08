108e62ad78deca8d09e4be12766ff7fe88f87ce6
uzucore <byunjaeil@gmail.com>
2024-11-08 16:37:43 +0900

    Update package.mk

--- packages/emulators/libretro/fbneo-xtreme-lr/package.mk
+++ packages/emulators/libretro/fbneo-xtreme-lr/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="fbneo-xtreme-lr"
-PKG_VERSION="9ce6a6cea5f5e2523de7e8fbf6d76f86719a932d"
+PKG_VERSION="0f0591b24a5438cfa00af96fee371cb59374b0b0"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/UzuCore/fbneo-xtreme"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
