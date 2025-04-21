c20f956149cf8274248b4c511c7be1393ca93e65
aleksei74 <aleksei74@gmail.com>
2025-04-22 08:29:58 +0900

    Update fbneo-dsno-lr (0421)

--- packages/emulators/libretro/fbneo-dsno-lr/package.mk
+++ packages/emulators/libretro/fbneo-dsno-lr/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="fbneo-dsno-lr"
-PKG_VERSION="3d04b0611a661c39822531a4f03e497bd3b2a49b"
+PKG_VERSION="21aae8b079c0d17c137e4aa45446d0fed092dff2"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/aleksei74/FBNeo"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
