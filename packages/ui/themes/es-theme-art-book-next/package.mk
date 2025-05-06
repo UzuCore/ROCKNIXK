6a3a386c317174db184c9c7be6494d8972cbbbc5
uzucore <byunjaeil@gmail.com>
2025-05-06 20:26:28 +0900

    테마

--- packages/ui/themes/es-theme-art-book-next/package.mk
+++ packages/ui/themes/es-theme-art-book-next/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="es-theme-art-book-next"
-PKG_VERSION="0b42bca86b8a07be559aed16b97a0d3d603eddf5"
+PKG_VERSION="a2cb4d3f5206c2af4c65ed0494bf0d3d89c6a7d7"
 PKG_LICENSE="CUSTOM"
-PKG_SITE="https://github.com/anthonycaccese/art-book-next-es"
+PKG_SITE="https://github.com/UzuCore/es-theme-art-book-dc"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Art Book Next"
 PKG_TOOLCHAIN="manual"
