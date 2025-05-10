a509904ba75320e8cd8ee2ef1ab1098fc379f2dc
uzucore <byunjaeil@gmail.com>
2025-05-11 02:45:26 +0900

    Update package.mk

--- packages/ui/themes/es-theme-art-book-next/package.mk
+++ packages/ui/themes/es-theme-art-book-next/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="es-theme-art-book-next"
-PKG_VERSION="a2cb4d3f5206c2af4c65ed0494bf0d3d89c6a7d7"
+PKG_VERSION="e36cbb2da4eb02e4e8edad0017afcc060e04a2a7"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/UzuCore/es-theme-art-book-dc"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
