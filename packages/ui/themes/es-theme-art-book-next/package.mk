195cc39a367c0655490119b5d91fe77013c9c67a
uzucore <byunjaeil@gmail.com>
2025-05-29 20:10:49 +0900

    테마

--- packages/ui/themes/es-theme-art-book-next/package.mk
+++ packages/ui/themes/es-theme-art-book-next/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="es-theme-art-book-next"
-PKG_VERSION="85c3ceeaa7f5ae4e2c3d069b9b1d587e586247d5"
+PKG_VERSION="435db0884ac4b5544b98ee0097cf8afb96dc4ef2"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/UzuCore/es-theme-art-book-dc"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
@@ -13,4 +13,9 @@ PKG_TOOLCHAIN="manual"
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/share/themes/${PKG_NAME}
     cp -rf * ${INSTALL}/usr/share/themes/${PKG_NAME}
+
+    rm -rf ${INSTALL}/usr/share/themes/${PKG_NAME}/_inc/systems/artwork-circuit
+    rm -rf ${INSTALL}/usr/share/themes/${PKG_NAME}/_inc/systems/artwork-classic
+    rm -rf ${INSTALL}/usr/share/themes/${PKG_NAME}/_inc/systems/artwork-noir
+    rm -rf ${INSTALL}/usr/share/themes/${PKG_NAME}/_inc/systems/artwork-outline
 }
