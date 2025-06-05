463fea45c22baaf69089bcc6079964066c52f63e
uzucore <byunjaeil@gmail.com>
2025-06-06 08:35:26 +0900

    노가다

---
+++ .hell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TARGET="AMD64 RK3588 RK3566 RK3326 RK3399 S922X SM8250 SM8550 H700"
+
+make clean
+
+for var in $TARGET
+do make $var;
+done
+
+./.clean.sh same_cdi-lr
+./.build.sh same_cdi-lr
+
+./.clean.sh mame-lr
+./.build.sh mame-lr
+
+for var in $TARGET
+do make $var;
+done
