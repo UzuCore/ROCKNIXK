b8e863c693323cb3af5ed41631a0d9628f191f77
uzucore <byunjaeil@gmail.com>
2025-06-18 00:27:13 +0900

    야망

--- packages/rocknix/package.mk
+++ packages/rocknix/package.mk
@@ -62,7 +62,7 @@ post_install() {
 ... Built: ${BUILD_DATE}
 
 EOF
-  #cp ${PKG_DIR}/sources/motd ${INSTALL}/etc
+  #cp ${PKG_DIR}/sources/motd ${INSTALL}/etc/issue
   #cat ${INSTALL}/etc/issue >> ${INSTALL}/etc/motd
 
   cp ${PKG_DIR}/sources/scripts/* ${INSTALL}/usr/bin
