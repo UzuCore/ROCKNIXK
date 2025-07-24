cd2f1c7edf9f64a76f4023f9cab3683fc0763253
aleksei74 <aleksei74@gmail.com>
2025-07-25 08:45:41 +0900

    Revert ScummVM 2.8.1 (#158)

--- projects/ROCKNIX/packages/emulators/standalone/scummvmsa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/scummvmsa/package.mk
@@ -3,16 +3,16 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="scummvmsa"
-PKG_VERSION="9d9b9e93108a276c551aeffa390169ccc5148e15" # 2.9.1
+PKG_VERSION="5f6ad051511dd8af5fb707efc2fd6b56834045d6" # 2.8.1
 PKG_LICENSE="GPL2"
-PKG_SITE="https://github.com/scummvm/scummvm"
+PKG_SITE="https://github.com/british-choi/scummvm"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain SDL2 SDL2_net freetype fluidsynth soundfont-generaluser pipewire libmad libtheora"
 PKG_LONGDESC="Script Creation Utility for Maniac Mansion Virtual Machine"
 
 pre_configure_target() { 
   sed -i "s|sdl-config|sdl2-config|g" ${PKG_BUILD}/configure
-  TARGET_CONFIGURE_OPTS="--host=${TARGET_NAME} --backend=sdl --disable-alsa --with-sdl-prefix=${SYSROOT_PREFIX}/usr/bin --disable-debug --enable-release --enable-vkeybd --opengl-mode=gles2 --enable-optimizations"
+  TARGET_CONFIGURE_OPTS="--host=${TARGET_NAME} --backend=sdl --disable-alsa --with-sdl-prefix=${SYSROOT_PREFIX}/usr/bin --disable-debug --enable-release --enable-vkeybd --enable-optimizations"
 }
 
 post_makeinstall_target() {
