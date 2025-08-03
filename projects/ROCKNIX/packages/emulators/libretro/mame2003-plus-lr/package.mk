de2306bb9b7aad0e1533848c44414bff0009f5cf
uzucore <byunjaeil@gmail.com>
2025-08-03 17:25:38 +0900

    코어 업데이트

--- projects/ROCKNIX/packages/emulators/libretro/mame2003-plus-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/mame2003-plus-lr/package.mk
@@ -21,7 +21,7 @@
 ################################################################################
 
 PKG_NAME="mame2003-plus-lr"
-PKG_VERSION="aabcb2ff407c8acee3abfee4c0002446c48c1037"
+PKG_VERSION="6699389c7168ef95115a108dcef696844ab8a76f"
 PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/UzuCore/mame2003-plus-dsno-libretro"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
