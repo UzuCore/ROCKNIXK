cad4bd2fe951930406ba9d24cec3edfedacec361
aleksei74 <aleksei74@gmail.com>
2025-10-26 21:41:15 +0900

    Update fbneo/eden/rpcs3/vulkan/emulationstation (#232)

--- projects/ROCKNIX/packages/emulators/libretro/fbneo-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/fbneo-lr/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="fbneo-lr"
-PKG_VERSION="559566561549eb407a226cc365302c54eea4c7f0"
+PKG_VERSION="c3ee681fec40428f4fd6c1073a4e17912aee68cf"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/aleksei74/FBNeo"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
