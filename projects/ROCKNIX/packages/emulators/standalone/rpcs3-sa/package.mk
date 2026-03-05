cef568abe760c5726207706b568634d206bd36e8
aleksei74 <aleksei74@gmail.com>
2026-03-06 02:21:04 +0900

    Update ppsspp/rpcs3 (#309)
    * Update ppsspp/rpcs3
    
    * Fix start_ppsspp.sh

--- projects/ROCKNIX/packages/emulators/standalone/rpcs3-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/rpcs3-sa/package.mk
@@ -7,8 +7,8 @@ PKG_SITE="https://github.com/RPCS3/rpcs3-binaries-linux"
 PKG_DEPENDS_TARGET="toolchain libevdev SDL2 qt6 mesa libcom-err"
 PKG_LONGDESC="PS3 Emulator appimage"
 PKG_TOOLCHAIN="manual"
-PKG_VERSION="aaf84a844542cf0697c19cb0d8579eff705b10c5"
-PKG_REL_VERSION="0.0.39-18800-aaf84a84"
+PKG_VERSION="86b2773c2854ede00ff376d1ee82898c74eb8b71"
+PKG_REL_VERSION="0.0.40-18874-86b2773c"
 
 case ${TARGET_ARCH} in
   x86_64)
