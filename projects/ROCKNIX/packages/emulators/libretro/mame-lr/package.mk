70b0f80da58306fa5c1fb6001a416d877a84a5a0
aleksei74 <aleksei74@gmail.com>
2026-04-03 08:08:14 +0900

    Update mame-lr/wine

--- projects/ROCKNIX/packages/emulators/libretro/mame-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/mame-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="mame-lr"
-PKG_VERSION="74731455f45c08d4f303c6d07c4f1cbf791d52b6" # lrmame 0.286
+PKG_VERSION="dd2bcd1dae606dbb15859d699d435b70bf9e755e" # lrmame 0.287
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame"
 PKG_URL="https://github.com/libretro/mame/archive/${PKG_VERSION}.tar.gz"
