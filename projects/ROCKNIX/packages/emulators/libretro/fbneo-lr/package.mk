e815596c6fbee2a1770c33f103a0586eff5c1c42
aleksei74 <aleksei74@gmail.com>
2026-04-08 17:56:39 +0900

    Update fbneo-lr (260408)
    PGM2 게임 추가

--- projects/ROCKNIX/packages/emulators/libretro/fbneo-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/fbneo-lr/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="fbneo-lr"
-PKG_VERSION="324e738831b7446feda5048506f6ef5365ca5326" # DsNo (260329)
+PKG_VERSION="0ac700918f57ff000a995aff50768110a8cde32d" # DsNo (260329)
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/aleksei74/FBNeo"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
