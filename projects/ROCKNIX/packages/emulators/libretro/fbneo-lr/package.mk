97cb17e37026a37027c76fd87a47128c3704e1f9
litania <litania@naver.com>
2026-05-16 07:29:25 +0900

    Bump up more recent commit of fbneo

--- projects/ROCKNIX/packages/emulators/libretro/fbneo-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/fbneo-lr/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="fbneo-lr"
-PKG_VERSION="2b6ae2b4b4050036a08595c570e7cde3165aab0b"
+PKG_VERSION="09e45081253540b11199d44aa1f9b419e621c13c"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/FBNeo"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
