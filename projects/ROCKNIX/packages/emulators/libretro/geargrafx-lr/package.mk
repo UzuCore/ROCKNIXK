eaa71f9bd5cf9877997c27fa614656bf69cb118c
litania <litania@naver.com>
2026-05-16 07:20:09 +0900

    Bump up Geargrafx and Gearlynx to work inufuto's homebrew games

--- projects/ROCKNIX/packages/emulators/libretro/geargrafx-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/geargrafx-lr/package.mk
@@ -15,7 +15,7 @@
 ################################################################################
 
 PKG_NAME="geargrafx-lr"
-PKG_VERSION="faaef77303eaab62fe65870589ae0487ea1a1aa8"
+PKG_VERSION="0cdce58f8fc069ba2251bacf6e4836bb9bd925ce"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/drhelius/Geargrafx"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
--- projects/ROCKNIX/packages/emulators/libretro/gearlynx-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/gearlynx-lr/package.mk
@@ -15,7 +15,7 @@
 ################################################################################
 
 PKG_NAME="gearlynx-lr"
-PKG_VERSION="61ed2e05c75419ebd906c1cc5d8c6077d57fda92"
+PKG_VERSION="da4f1a7e14301778963eb57534acf8f161f46dae"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/drhelius/Gearlynx"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
